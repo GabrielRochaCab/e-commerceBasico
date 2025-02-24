4fc51c7e47737b7b1c291ae81031a31e3dfcdcd3
GabrielRocha <gbrochacabral@gmail.com>
2025-02-24 17:03:50 -0300

    Configuração incial do servidor e com banco de dados mysql

--- back-end/src/routes/productsRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const productControllers = require('../controllers/productsController');
-
-router.get('/', productControllers.listProduct);
-
-module.exports = router;
